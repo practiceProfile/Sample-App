4b2265d60216cb48567d28d1bdf6bc8187ded65c
Aaron Vasquez <Aaron@Macbook.local>
2012-07-26 21:05:58 -0400

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # returns the full title per page
+  def full_title(page_title)
+    base_title = "Aaron Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
